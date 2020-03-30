72264ab620bd218586bb6fb9a4e696b60a28238f
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-30 02:56:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ function logWhisper(string) {
   }
 
 function sayHiToGrandma(string){
-  return "I love you, too."
+  if string ==="I love too"{
+    return "I love you, too."}
 }
 
 funtion sayHiToGrandma(string){
